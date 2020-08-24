1e84fa7b47ba32188a16f824acbec2146db76abf
Iago Diniz <iagoo_ptu@hotmail.com>
2020-08-23 21:51:12 -0300

    Finalização

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,82 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repositore = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repositore)
+
+  return response.json(repositore)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id == id)
+
+  if (repositoreIndex < 0) {
+    return response.status(400).json({ error: 'repositore not found.' })
+  }
+
+  let repositore = repositories[repositoreIndex]
+
+  repositore.title = title
+  repositore.url = url
+  repositore.techs = techs
+
+
+  repositories[repositoreIndex] = repositore
+
+  return response.json(repositore)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id == id)
+
+  if (repositoreIndex < 0) {
+    return response.status(400).json({ error: 'repositore not found.' })
+  }
+
+  repositories.splice(repositoreIndex, 1)
+
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id == id)
+
+  if (repositoreIndex < 0) {
+    return response.status(400).json({ error: 'repositore not found.' })
+  }
+
+  let repositore = repositories[repositoreIndex]
+
+  repositore.likes = repositore.likes + 1
+
+  repositories[repositoreIndex] = repositore
+
+  return response.json(repositore)
+
 });
 
 module.exports = app;
